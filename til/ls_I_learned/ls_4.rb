d900f7054a6de3813aed81d0e7633fdf868846e8
OdenTakashi <odentakashi@icloud.com>
2022-01-20 15:56:41 +0900

    オーナー名、グループ名の表示

--- til/ls_I_learned/ls_4.rb
+++ til/ls_I_learned/ls_4.rb
@@ -7,3 +7,16 @@ puts stat.mtime.to_s.slice!(6..15)
 puts stat.mtime
 #=> 2022-01-12 12:05:48 +0900
 # この情報を月日、時間（分まで）にするためにslice(6..15)を指定した
+
+require 'etc'
+
+user_id    = Process.uid
+user_name  = Etc.getpwuid(user_id).name
+group_id   = Process.gid
+group_name = Etc.getgrgid(group_id).name
+
+puts user_name
+puts group_name
+
+#=> kodamanaoki
+#=> staff
