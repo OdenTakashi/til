e448510dffc603bf2b6bd9582ebbc75e47d7ed65
OdenTakashi <odentakashi@icloud.com>
2022-01-26 15:10:50 +0900

    ファイルパーミッション追加

--- til/ls_I_learned/ls_4.rb
+++ til/ls_I_learned/ls_4.rb
@@ -1,6 +1,8 @@
 stat = File::Stat.new($0)
 # puts stat.mtime.to_s.slice!(6..15)
-p stat.mode
+# p stat.mode.to_s(8)
+# -rw-r--r--
+# 100644
 #=> 1-12 12:05
 #=> 1-20 14:54
 
@@ -15,7 +17,7 @@ user_name  = Etc.getpwuid(user_id).name
 group_id   = Process.gid
 group_name = Etc.getgrgid(group_id).name
 
-puts "#{stat.nlink} #{user_name} #{group_name}  #{File.size('ls_4.rb')} #{stat.mtime.to_s.slice!(6..15)}"
+puts "#{stat.mode.to_s(8)} #{stat.nlink} #{user_name} #{group_name}  #{File.size('ls_4.rb')} #{stat.mtime.to_s.slice!(6..15)}"
 
 #=> kodamanaoki
 #=> staff
